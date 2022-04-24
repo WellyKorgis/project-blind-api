180d39ac9d5364b266c749e03574ac5cac568bf3
seul7i <seulgi.lee01@student.weltec.ac.nz>
2022-04-24 17:13:56 +1200

    Add the post category domain and its repository

--- src/main/java/com/blind/api/post/domain/PostCategory.java
+++ src/main/java/com/blind/api/post/domain/PostCategory.java
@@ -1,2 +1,23 @@
-package com.blind.api.post.domain;public class PostCategory {
+package com.blind.api.post.domain;
+
+import com.blind.common.domain.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "post_category")
+public class PostCategory extends BaseEntity {
+    @Column(nullable = false)
+    private String name;
 }
