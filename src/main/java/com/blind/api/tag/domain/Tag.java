9e0301e7894463a8f335308c62e8fabfa2976f90
seul7i <seulgi.lee01@student.weltec.ac.nz>
2022-04-24 17:08:15 +1200

    Add the tag domain and its repository

--- src/main/java/com/blind/api/tag/domain/Tag.java
+++ src/main/java/com/blind/api/tag/domain/Tag.java
@@ -1,2 +1,23 @@
-package com.blind.api.tag.domain;public class Tag {
+package com.blind.api.tag.domain;
+
+import com.blind.api.user.domain.User;
+import com.blind.common.domain.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "tags")
+public class Tag extends BaseEntity {
+    @Column(nullable = false)
+    private String name;
 }
--- src/main/java/com/blind/api/tag/repository/TagRepository.java
+++ src/main/java/com/blind/api/tag/repository/TagRepository.java
@@ -1,2 +1,10 @@
-package com.blind.api.tag.repository;public class TagRepository {
+package com.blind.api.tag.repository;
+
+import com.blind.api.post.domain.Post;
+import com.blind.api.tag.domain.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Integer> {
 }
