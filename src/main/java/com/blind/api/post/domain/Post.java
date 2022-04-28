9d5fc0605c7c69a4c57e4140f82cd62c7a3321d1
Seulgi Lee <seulgi.lee01@student.weltec.ac.nz>
2022-04-28 19:06:35 +1200

    Change userid field as user and add postcategory as a new memeber field

--- src/main/java/com/blind/api/post/domain/Post.java
+++ src/main/java/com/blind/api/post/domain/Post.java
@@ -2,10 +2,12 @@ package com.blind.api.post.domain;
 
 import com.blind.api.user.domain.User;
 import com.blind.common.domain.BaseEntity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
@@ -14,20 +16,34 @@ import java.time.OffsetDateTime;
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 @Table(name = "posts")
 public class Post extends BaseEntity {
     @Column(nullable = false)
     private String title;
+
     @Column(nullable = false)
     private String content;
+
     @CreatedDate
     @Column(name = "created_at", nullable = false)
-    private OffsetDateTime createAt;
+    private OffsetDateTime createdAt;
+
     @JoinColumn(nullable = false)
     @ManyToOne
     private User user;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne
+    private PostCategory postCategory;
+
+    @LastModifiedBy
     @Column(name = "updated_at", nullable = false)
     private OffsetDateTime updatedAt;
+
+    public Post(String title, String content, User user) {
+        this.title = title;
+        this.content = content;
+        this.user = user;
+    }
 }
