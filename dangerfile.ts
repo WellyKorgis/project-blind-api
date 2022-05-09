d0a9921f9f87a24734b7234da2c5c2b1f217c613
nmsohn <odna425@gmail.com>
2022-05-09 18:36:56 +1200

    Setup github action for danger.js
    This allows danger.js to scan a new PR and prevents any violation for good PRs

---
+++ dangerfile.ts
@@ -0,0 +1,25 @@
+import { danger, message, warn } from 'danger';
+
+var isGood = true;
+
+const diffSize = Math.max(danger.github.pr.additions, danger.github.pr.deletions);
+if (diffSize > 300 ) {
+    warn("This PR is too big. You should divide this PR into smaller PRs.");
+    isGood = false;
+}
+
+if (danger.github.pr.changed_files > 10 ) {
+    warn("This PR changes too many files. You should divide this PR into smaller PRs.");
+    isGood = false;
+}
+
+for (const commit of danger.github.commits) {
+    if (commit.commit.message.length < 5) {
+        warn("There is a commit with very short message: " +  commit.commit.message)
+        isGood = false;
+    }
+}
+
+if (isGood) {
+    message("This PR looks good :)")
+}
