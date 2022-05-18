4ef0c188932088f5eff900621b77920ad2415227
seegya <77556070+seegya@users.noreply.github.com>
2022-05-18 18:43:13 +1200

    Update PostController.java
    Add a controller(POST, GET method)

--- src/main/java/com/blind/post/api/PostController.java
+++ src/main/java/com/blind/post/api/PostController.java
@@ -4,4 +4,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PostController {
+    private final PostRepository postRepository;
+
+    public PostController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @GetMapping("/")
+    String hi() {
+        return "hi";
+    }
+
+    @GetMapping("/posts")
+    List<Post> listAll() {
+        return postRepository.findAll();
+    }
+
+    @PostMapping("/post")
+    Post createPost(@RequestBody Post post) {
+        postRepository.save(post);
+        return post;
+    }
 }
