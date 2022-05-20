0dc315888fe300f6f635e1a5d27d0092cd5fc417
nmsohn <odna425@gmail.com>
2022-05-20 18:40:03 +1200

    Update repositories in company domain
    When finding by id, they take UUID as a parameter

--- src/main/java/com/blind/company/persistence/respository/JobPositionRepository.java
+++ src/main/java/com/blind/company/persistence/respository/JobPositionRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface JobPositionRepository extends JpaRepository<JobPosition, Integer> {
-    Optional<JobPosition> findById(String id);
+public interface JobPositionRepository extends JpaRepository<JobPosition, UUID> {
+    Optional<JobPosition> findById(UUID id);
 }
