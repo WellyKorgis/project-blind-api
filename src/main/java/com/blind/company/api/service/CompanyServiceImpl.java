c95ee90d95e71c4627cd9c6e79e942355b2b2ef3
nmsohn <odna425@gmail.com>
2022-05-20 20:08:21 +1200

    Add getCompany in CompanyService implementation

--- src/main/java/com/blind/company/api/service/CompanyServiceImpl.java
+++ src/main/java/com/blind/company/api/service/CompanyServiceImpl.java
@@ -1,2 +1,29 @@
-package com.blind.company.api.service;public class CompanyServiceImpl {
+package com.blind.company.api.service;
+
+import com.blind.company.api.dto.response.GetCompanyResponse;
+import com.blind.company.api.mapper.CompanyMapper;
+import com.blind.company.domain.Company;
+import com.blind.company.persistence.respository.CompanyRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Slf4j
+@Service()
+@RequiredArgsConstructor
+public class CompanyServiceImpl implements CompanyService {
+
+    private final CompanyRepository companyRepository;
+    private final CompanyMapper companyMapper;
+
+    @Override
+    public GetCompanyResponse getCompany(UUID id) {
+        Company company = companyRepository.findById(id).orElse(null);
+
+        if (company == null) return null;
+
+        return companyMapper.INSTANCE.companyToGetCompanyResponse(company);
+    }
 }
