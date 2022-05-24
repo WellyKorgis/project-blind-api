09470e86845884f48d7b744a5647b4d563f0acf8
nmsohn <odna425@gmail.com>
2022-05-24 18:18:22 +1200

    Housekeeping: Remove comments

--- src/main/java/com/blind/company/api/mapper/CompanyMapper.java
+++ src/main/java/com/blind/company/api/mapper/CompanyMapper.java
@@ -8,8 +8,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
-//@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
-//@DecoratedWith(CompanyMapperDecorator.class)
 @Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CompanyMapper {
     CompanyMapper INSTANCE = Mappers.getMapper(CompanyMapper.class);
