4543590516963f2879485f62235a4abaa82ded68
seul7i <seulgi.lee01@student.weltec.ac.nz>
2022-05-25 22:26:37 +1200

    Update contorller endpoint

--- src/main/java/com/blind/post/api/PostController.java
+++ src/main/java/com/blind/post/api/PostController.java
@@ -2,6 +2,7 @@ package com.blind.post.api;
 
 import com.blind.post.domain.Post;
 import com.blind.post.persistence.repository.PostRepository;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+
+@RestController("/v1/post")
 public class PostController {
     private final PostRepository postRepository;
 
@@ -19,12 +21,12 @@ public class PostController {
         this.postRepository = postRepository;
     }
 
-    @GetMapping("/posts")
+    @GetMapping("/")
     List<Post> listAll() {
         return postRepository.findAll();
     }
 
-    @PostMapping("/post")
+    @PostMapping("/")
     Post createPost(@RequestBody Post post) {
         postRepository.save(post);
         return post;
