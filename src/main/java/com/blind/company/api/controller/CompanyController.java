e65f294441706326a9c87f624f575ff957eafcda
nmsohn <odna425@gmail.com>
2022-05-26 11:29:28 +1200

    Update uri in company controller

--- src/main/java/com/blind/company/api/controller/CompanyController.java
+++ src/main/java/com/blind/company/api/controller/CompanyController.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 @Slf4j
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/api/company")
+@RequestMapping("/company")
 public class CompanyController {
 
     private final CompanyService companyService;
