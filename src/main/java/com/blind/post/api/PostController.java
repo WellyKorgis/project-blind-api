a0edb0657c28c3d2e5f437659b63a8ccb26c8b85
seegya <77556070+seegya@users.noreply.github.com>
2022-06-09 12:04:23 +1200

    Seegya/feature/swagger (#64)
    Co-authored-by: seul7i <seulgi.lee01@student.weltec.ac.nz>
    Co-authored-by: nmsohn <odna425@gmail.com>

--- src/main/java/com/blind/post/api/PostController.java
+++ src/main/java/com/blind/post/api/PostController.java
@@ -2,6 +2,7 @@ package com.blind.post.api;
 
 import com.blind.post.domain.Post;
 import com.blind.post.persistence.repository.PostRepository;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+
+@RestController("/api/v1")
 public class PostController {
     private final PostRepository postRepository;
 
@@ -19,11 +21,21 @@ public class PostController {
         this.postRepository = postRepository;
     }
 
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "NOT_FOUND"),
+            @ApiResponse(code = 500, message = "INTERNAL_SERVER_ERROR"),
+    })
     @GetMapping("/posts")
     List<Post> listAll() {
         return postRepository.findAll();
     }
 
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "BAD_REQUEST"),
+            @ApiResponse(code = 500, message = "INTERNAL_SERVER_ERROR"),
+    })
     @PostMapping("/post")
     Post createPost(@RequestBody Post post) {
         postRepository.save(post);
