e6c1dfe81145f93c8815868e4b7b37ffe6cbf483
Seulgi Lee <seulgi.lee01@student.weltec.ac.nz>
2022-06-11 19:35:13 +1200

    Implement command line runner to add data using jpa

--- src/main/java/com/blind/BlindApplication.java
+++ src/main/java/com/blind/BlindApplication.java
@@ -1,11 +1,29 @@
 package com.blind;
 
+import com.blind.company.domain.CompanyCategory;
+import com.blind.company.persistence.respository.CompanyCategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class BlindApplication {
+public class BlindApplication implements CommandLineRunner {
+    @Autowired
+    CompanyCategoryRepository ccRepo;
     public static void main(String... args) {
         SpringApplication.run(BlindApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        ccRepo.saveAll(List.of(
+                new CompanyCategory("Hospitality"),
+                new CompanyCategory("IT")
+            )
+        );
+        System.out.println("data inserted");
     }
 }
