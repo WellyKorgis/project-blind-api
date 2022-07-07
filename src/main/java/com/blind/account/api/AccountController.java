d7343ab2f49ae0e3055fca0a3964afa91b9ff4fb
Seulgi Lee <seulgi.lee01@student.weltec.ac.nz>
2022-07-07 21:04:25 +1200

    Add createPost in controller

--- src/main/java/com/blind/account/api/AccountController.java
+++ src/main/java/com/blind/account/api/AccountController.java
@@ -1,7 +1,42 @@
 package com.blind.account.api;
 
+import com.blind.account.domain.Account;
+import com.blind.account.persistence.repository.AccountRepository;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.validation.Valid;
+import java.util.Optional;
+
+import static java.awt.SystemColor.info;
+
+@Slf4j
+@RestController("/api/v1/accounts")
 public class AccountController {
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountController(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 204, message = "NO_CONTENT"),
+            @ApiResponse(code = 404, message = "NOT_FOUND"),
+            @ApiResponse(code = 500, message = "INTERNAL_SERVER_ERROR"),
+    })
+    @GetMapping()
+    ResponseEntity<String> addAccount(@Valid @RequestBody Account accountToAdd) {
+        return accountRepository.save(accountToAdd);
+    }
+
 }
