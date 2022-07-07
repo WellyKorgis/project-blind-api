3a6bdc3649c635176b3b774fbb406827d2e95d8a
Seulgi Lee <seulgi.lee01@student.weltec.ac.nz>
2022-07-07 19:51:13 +1200

    Add account api endpoint

--- src/main/java/com/blind/account/api/AccountController.java
+++ src/main/java/com/blind/account/api/AccountController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController("/api/v1/accounts")
 public class AccountController {
     private final AccountRepository accountRepository;
 
