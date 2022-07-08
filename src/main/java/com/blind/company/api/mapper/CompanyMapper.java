173e99280e5c5c4553588c57131c98b5b53c814d
nmsohn <odna425@gmail.com>
2022-07-09 11:33:41 +1200

    Add GET company list service
    Implementation of GET company list in service. It's using spring boot data Page for pagination. This will be replaced by a custom one in a separate ticket

--- src/main/java/com/blind/company/api/mapper/CompanyMapper.java
+++ src/main/java/com/blind/company/api/mapper/CompanyMapper.java
@@ -8,10 +8,13 @@ import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CompanyMapper {
     CompanyMapper INSTANCE = Mappers.getMapper(CompanyMapper.class);
 
     CompanyResponse companyToDto (Company company);
     CompanyCategoryResponse companyCategoryToDto (CompanyCategory companyCategory);
+    List<CompanyResponse> companyListToDto(List<Company> companyList);
 }
