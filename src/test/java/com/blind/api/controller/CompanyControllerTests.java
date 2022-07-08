e3ef370e48b3d8c03eafe51db5d4a0ab26bb0d5f
nmsohn <odna425@gmail.com>
2022-07-09 11:34:22 +1200

    Add GET company list in controller
    Company controller unit test is also added

--- src/test/java/com/blind/api/controller/CompanyControllerTests.java
+++ src/test/java/com/blind/api/controller/CompanyControllerTests.java
@@ -13,8 +13,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static org.hamcrest.Matchers.is;
@@ -45,10 +51,37 @@ public class CompanyControllerTests {
 
         given(companyService.getCompany(company.getId())).willReturn(companyDto);
 
-        this.mockMvc.perform(get("/company/{id}", company.getId()))
+        this.mockMvc.perform(get("/companies/{id}", company.getId()))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", is(company.getName())));
 
         verify(companyService).getCompany(company.getId());
+    }
+
+    @Test
+    public void getCompanyList() throws Exception {
+
+        final CompanyCategory companyCategory = new CompanyCategory(UUID.randomUUID(), "CompanyCategory");
+
+        final List<Company> companyList = new ArrayList<>();
+
+        companyList.add(new Company(UUID.randomUUID(), "Company1", companyCategory));
+        companyList.add(new Company(UUID.randomUUID(), "Company2", companyCategory));
+        companyList.add(new Company(UUID.randomUUID(), "Company3", companyCategory));
+
+        final List<CompanyResponse> companyResponses = companyMapper.INSTANCE.companyListToDto(companyList);
+
+        Pageable pageable = PageRequest.of(0, 20);
+        Page<CompanyResponse> pagedResponses = new PageImpl<>(companyResponses);
+
+        given(companyService.getCompanyList(pageable)).willReturn(pagedResponses);
+
+        this.mockMvc.perform(get("/companies"))
+                .andExpect(jsonPath("$.content.[0].name", is("Company1")))
+                .andExpect(jsonPath("$.content.[1].name", is("Company2")))
+                .andExpect(jsonPath("$.content.[2].name", is("Company3")))
+                .andExpect(status().isOk());
+
+        verify(companyService).getCompanyList(pageable);
     }
 }
