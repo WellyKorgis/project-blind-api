b9ea225406d9bfc5bf4e2a61d2c7f757d2e7aac0
Seulgi Lee <seulgi.lee01@student.weltec.ac.nz>
2022-07-14 19:54:16 +1200

    Add post DTO for GET method response

---
+++ src/main/java/com/blind/post/dto/response/getPostDto.java
@@ -0,0 +1,21 @@
+package com.blind.post.dto.response;
+
+import com.blind.account.domain.Account;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+public class getPostDto {
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("createdAt")
+    private OffsetDateTime createAt;
+
+    @JsonProperty("createdBy")
+    private Account account;
+}
